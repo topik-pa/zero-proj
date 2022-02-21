e7d275bba202aa716e2b1b15e634aad29f570d94
Marco Pavan <marco.pavan@vivabiocell.it>
2022-02-21 17:53:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use('/scripts', express.static(__dirname + '/scripts'))
+app.use('/styles', express.static(__dirname + '/styles'))
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Marco' })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
