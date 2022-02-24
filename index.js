946ff5b08701b7192b30dc2bd27aea4a82db48e1
Marco Pavan <marco.pavan@vivabiocell.it>
2022-02-24 07:30:53 +0100

    sitemap + new routes + 404 + logo + meta tags + other

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use('/public', express.static(__dirname + '/public'))
 app.use('/components', express.static(__dirname + '/components'))
 app.use('/scripts', express.static(__dirname + '/scripts'))
 app.use('/styles', express.static(__dirname + '/styles'))
+app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' })
@@ -19,6 +20,9 @@ app.get('/privacy', (req, res) => {
 app.get('/contatti', (req, res) => {
   res.render('contatti', { title: 'Contatti' })
 })
+app.get('*', function(req, res){
+  res.render('404', { title: 'Error 404' })
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
