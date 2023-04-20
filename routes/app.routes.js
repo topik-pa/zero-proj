840231db50f2b6ef9f112765140c43bef9aadd0c
Marco Pavan <marco.pavan@vivabiocell.it>
2023-04-20 09:50:38 +0200

    Users view added

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -2,6 +2,14 @@ module.exports = app => {
   app.get('/', (req, res) => {
     res.render('home', { id: 'home', title: 'Home', url: req.url })
   })
+  app.get('/users', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'users'
+      }
+    ]
+    res.render('users/users', { id: 'users', title: 'Users', url: req.url, breadcrumbs })
+  })
   app.get('/privacy', (req, res) => {
     const breadcrumbs = [
       {
---
+++ views/users/users.js
@@ -0,0 +1,7 @@
+const users = {
+  init: async () => {
+    console.log('Users')
+  }
+}
+
+export default users
