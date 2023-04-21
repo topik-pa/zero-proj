c22c0851313fd447f82b6462f16e295e2e11bd2f
Marco Pavan <marco.pavan@vivabiocell.it>
2023-04-21 09:55:33 +0200

    Full pagination working

--- components/shared/dTable/dTable.js
+++ components/shared/dTable/dTable.js
@@ -1,11 +1,56 @@
 const url = 'https://dummyjson.com/users'
 const $table = document.getElementById('dtable')
+const $pagination = $table.querySelector('#pagination')
+const $navigation = $table.querySelector('ul')
 
-const buildTable = (rows) => {
+const initConf = {
+  limit: 7,
+  page: 1
+}
+
+const queryString = {
+  skip: initConf.page * initConf.limit,
+  limit: initConf.limit,
+  sortBy: undefined,
+  order: undefined,
+  filters: [],
+  sorts: [],
+  get () {
+    let qs = '?'
+    qs += `skip=${this.skip}`
+    qs += `&limit=${this.limit}`
+    /* this.sorts.forEach(sort => {
+      qs += `&sort=${encodeURIComponent(sort.order + sort.sortBy)}`
+    })
+    this.filters.forEach(f => {
+      const like = f.filter.charAt(f.filter.length - 1) === ']' ? '' : '%' // TODO: not proud...
+      qs += `&${encodeURIComponent(f.col)}=${encodeURIComponent(f.filter)}${like}`
+    }) */
+    return qs
+  }
+}
+
+const pagination = {
+  total: undefined,
+  partial: queryString.limit,
+  pages: undefined,
+  page: initConf.page
+}
+
+// let $selectedPage = $table.querySelector(`li[data-page=${pagination.page}]`)
+
+$navigation.addEventListener('click', async (e) => {
+  const nextPage = --e.target.dataset.page
+  queryString.skip = nextPage * queryString.limit
+  pagination.page = nextPage
+  await loadRowsData()
+})
+
+const buildRows = (rows) => {
   const $tBody = $table.getElementsByTagName('tbody')[0]
-  const $tCaption = $table.querySelector('caption')
+  // const $tCaption = $table.querySelector('caption')
   $tBody.innerText = ''
-  $tCaption.innerText += ' ' + rows.length
+  // $tCaption.innerText += ' ' + rows.length
   for (const row of rows) {
     const $tr = document.createElement('tr')
     $tr.innerHTML = `
@@ -22,20 +67,48 @@ const buildTable = (rows) => {
   }
 }
 
+const updateDOMPagination = () => {
+  $pagination.querySelector('#total').innerText = pagination.total
+  $pagination.querySelector('#partial').innerText = pagination.partial
+  $pagination.querySelector('#page').innerText = (pagination.page + 1)
+  $pagination.querySelector('#pages').innerText = pagination.pages
+}
+
+const setNavigation = () => {
+  $navigation.innerText = ''
+  for (let i = 0; i < pagination.pages; i++) {
+    const $li = document.createElement('li')
+    $li.innerHTML = `${(i + 1)}`
+    $li.setAttribute('data-page', (i + 1))
+    if (i === pagination.page) $li.classList.add('active')
+    $navigation.appendChild($li)
+  }
+}
+
+const loadRowsData = async () => {
+  $table.classList.toggle('loading')
+  await fetch(url + queryString.get())
+    .then(response => response.json())
+    .then((json) => {
+      buildRows(json.users)
+      pagination.total = json.total
+      pagination.pages = Math.ceil(json.total / queryString.limit)
+      // if (pagination.pages === 0) pagination.pages++ // Do not display "0" for page
+      updateDOMPagination()
+      setNavigation()
+    })
+    .catch((error) => {
+      $table.classList.add('error')
+      console.error(error)
+    })
+    .finally(() => {
+      $table.classList.toggle('loading')
+    })
+}
+
 const dTable = {
   init: async () => {
-    $table.classList.toggle('loading')
-    await fetch(url)
-      .then(response => response.json())
-      .then((json) => {
-        buildTable(json.users)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-      .finally(() => {
-        $table.classList.toggle('loading')
-      })
+    await loadRowsData()
   }
 }
 
