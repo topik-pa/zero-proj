8b6b992288f35da6c94e8130dcc254c3855c76d0
Marco Pavan <marcopavan.mp@gmail.com>
2024-05-02 08:53:49 +0200

    Some upgrade from other projs

--- index.js
+++ index.js
@@ -2,8 +2,43 @@ const express = require('express')
 const app = express()
 const { I18n } = require('i18n')
 const path = require('path')
+const compression = require('compression')
 require('dotenv').config()
+const crypto = require('crypto')
 const PORT = process.env.PORT || 3000
+
+// HTTPS redirect server-side
+/* app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + req.headers.host + req.url)
+    } else { return next() }
+  } else { return next() }
+}) */
+
+// Compress responses if browser is capable
+app.use(compression({ filter: shouldCompress }))
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
+// Add nonce attribute
+const nonce = crypto.randomUUID()
+// Set custom headers
+app.use(function (req, res, next) {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+  res.setHeader('Upgrade-insecure-requests', '1')
+  // eslint-disable-next-line max-len
+  // res.setHeader('Content-Security-Policy', `default-src 'none'; script-src https://connect.facebook.net/ https://www.statcounter.com/ https://platform.linkedin.com/ https://www.linkedin.com/ https://cpwebassets.codepen.io/ 'self' 'nonce-${nonce}'; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.paypalobjects.com/ https://www.paypal.com/ https://c.statcounter.com/ data:; object-src 'none'; frame-src https://c.statcounter.com/ https://www.facebook.com/ https://web.facebook.com/ https://www.linkedin.com/ https://codepen.io/ 'self'; form-action https://www.paypal.com/ 'self'; font-src 'self'; media-src 'self'; connect-src https://c.statcounter.com/ 'self'; frame-ancestors 'none'; base-uri 'none'`)
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'deny')
+  next()
+})
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -33,13 +68,14 @@ app.locals.basedir = path.join(__dirname, '/')
 
 // ROUTES
 require('./routes/api.routes')(app)
-require('./routes/app.routes')(app)
+require('./routes/app.routes')(app, nonce)
 // Handle 404
 app.use(function (req, res) {
   res.status(400)
   res.render('404/404', { id: 'err404', title: 'Error 404' })
 })
 // Handle 500
+// eslint-disable-next-line no-unused-vars
 app.use(function (error, req, res, next) {
   console.log(error.stack)
   res.status(500)
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,7 +1,7 @@
-module.exports = app => {
+module.exports = (app, nonce) => {
   app.get('/', (req, res) => {
     const title = 'Home'
-    res.render('home', { id: 'home', className: 'home', title, url: req.url })
+    res.render('home', { id: 'home', className: 'home', title, url: req.url, nonce })
   })
   app.get('/users', (req, res) => {
     const title = 'Users'
@@ -10,7 +10,7 @@ module.exports = app => {
         name: title
       }
     ]
-    res.render('users/users', { id: 'users', title, url: req.url, breadcrumbs })
+    res.render('users/users', { id: 'users', title, url: req.url, breadcrumbs, nonce })
   })
   app.get('/form', (req, res) => {
     const title = 'Form'
@@ -19,7 +19,7 @@ module.exports = app => {
         name: title
       }
     ]
-    res.render('form/form', { id: 'form', title, url: req.url, breadcrumbs })
+    res.render('form/form', { id: 'form', title, url: req.url, breadcrumbs, nonce })
   })
   app.get('/plp', (req, res) => {
     const title = 'PLP'
@@ -28,7 +28,7 @@ module.exports = app => {
         name: title
       }
     ]
-    res.render('plp/plp', { id: 'plp', title, url: req.url, breadcrumbs })
+    res.render('plp/plp', { id: 'plp', title, url: req.url, breadcrumbs, nonce })
   })
   app.get('/contacts', (req, res) => {
     const title = 'Contacts'
@@ -37,7 +37,7 @@ module.exports = app => {
         name: title
       }
     ]
-    res.render('contacts/contacts', { id: 'contacts', title, url: req.url, breadcrumbs })
+    res.render('contacts/contacts', { id: 'contacts', title, url: req.url, breadcrumbs, nonce })
   })
   app.get('/privacy', (req, res) => {
     const title = 'Privacy'
@@ -46,6 +46,14 @@ module.exports = app => {
         name: title
       }
     ]
-    res.render('privacy/privacy', { id: 'privacy', title, url: req.url, breadcrumbs })
+    res.render('privacy/privacy', { id: 'privacy', title, url: req.url, breadcrumbs, nonce })
+  })
+  // Sitemap.xml
+  app.get('/sitemap.xml', function (req, res) {
+    res.sendFile('public/sitemap.xml', { root: '.' })
+  })
+  // Robots.txt
+  app.get('/robots.txt', function (req, res) {
+    res.sendFile('public/robots.txt', { root: '.' })
   })
 }
